2dacc0bd5ee3e4d9e11bd1b104b5f17a26e44c19
Freccialata <33099442+Freccialata@users.noreply.github.com>
2023-04-27 18:30:19 +0200

    new page

---
+++ _app/immutable/chunks/0.b3d56ec0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_layout.svelte.5213dece.js";export{t as component};
---
+++ _app/immutable/chunks/4.831078fa.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.fca24c55.js";import{default as t}from"../entry/works-page.svelte.ca9464da.js";export{t as component,r as universal};
